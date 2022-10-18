1f135412ff4ec0c68f57619cc98d39252af54313
Andrea Burr <burrandy18@gmail.com>
2022-10-18 09:54:59 -0700

    first push

---
+++ tryagain/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World hope this will link to git!");
+
+  git config global user.email "burrandy18@gmail.com";
+  git config global user.name "Andrea Burr";
